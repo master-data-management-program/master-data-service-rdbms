4c42ee3559046ddb297b514fd13eb8966f6f3c52
baira8ms <baira8ms@gmail.com>
2020-01-07 23:14:15 +0000

    Adding swagger api info end point

--- src/main/java/com/master/data/management/config/SpringFoxConfig.java
+++ src/main/java/com/master/data/management/config/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.master.data.management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,16 @@ public class SpringFoxConfig {
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiEndPointsInfo());
+  }
+
+  private ApiInfo apiEndPointsInfo() {
+    return new ApiInfoBuilder().title("Spring Boot REST API")
+        .description("Master Data Management REST API")
+        .contact(new Contact("Chiranjeevi Bairaagoni", "localhost:2101", "baira8ms@gmail.com"))
+        .license("MDM Project")
+        .version("1.0.0")
         .build();
   }
 }
