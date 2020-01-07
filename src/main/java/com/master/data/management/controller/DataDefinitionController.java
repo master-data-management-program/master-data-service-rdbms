935a4fe9187fee429f016f29cf738988f9f1dbe7
baira8ms <baira8ms@gmail.com>
2020-01-07 23:34:40 +0000

    Adding swagger api info end point

--- src/main/java/com/master/data/management/controller/DataDefinitionController.java
+++ src/main/java/com/master/data/management/controller/DataDefinitionController.java
@@ -5,6 +5,8 @@ import static org.springframework.http.HttpStatus.OK;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.master.data.management.dto.DataModelResponse;
 import com.master.data.management.service.DataModelsOrchestrationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("This controller endpoints are used to (DDL) create and manage tables")
 @RequestMapping("${data.endpoint}/tables")
 @RestController
 @Slf4j
@@ -24,6 +27,7 @@ public class DataDefinitionController {
   @Autowired
   private DataModelsOrchestrationService orchestrationService;
 
+  @ApiOperation(value = "Create new entity endpoint to create table in database witht the provided json request.", consumes = "application/hal+json", produces = "application/json")
   @PostMapping(produces = "application/json", consumes = "application/hal+json")
   public ResponseEntity<String> createNewEntity(
       @RequestBody JsonNode requestJson) throws Exception {
@@ -40,56 +44,18 @@ public class DataDefinitionController {
         .body(dataModelResponse.getStatusMessage());
   }
 
+  @ApiOperation(value = "Retrieves all tablenames which are active in the database.", produces = "application/json")
   @GetMapping(produces = "application/json")
   public ResponseEntity<List<String>> getTablesList() {
     List<String> tablesList = orchestrationService.getTablesList();
     return ResponseEntity.status(OK).body(tablesList);
   }
 
+  @ApiOperation(value = "Retrieves all fields names from the database,for the provided table name.", produces = "application/json")
   @GetMapping(value = "{tableName}/fields", produces = "application/json")
   public ResponseEntity<List<String>> getFieldsByTableName(
       @PathVariable("tableName") String tableName) {
     List<String> fieldsList = orchestrationService.getFieldsByTableName(tableName);
     return ResponseEntity.status(OK).body(fieldsList);
   }
-
-//  @PutMapping
-//  public ResponseEntity<String> updateEntity(
-//      @RequestParam("tableName") String collectionName,
-//      @RequestBody JsonNode requestJson) {
-//
-//    DataModelResponse dataModelResponse = orchestrationService
-//        .updateDataModel(collectionName, requestJson);
-//
-//    return ResponseEntity
-//        .status(dataModelResponse.getHttpStatus())
-//        .body(dataModelResponse.getStatusMessage());
-//  }
-//
-//
-//  @PutMapping
-//  public ResponseEntity<String> updateEntity(
-//      @RequestParam("tableName") String collectionName,
-//      @RequestParam("indexId") String indexId,
-//      @RequestBody JsonNode requestJson) {
-//
-//    DataModelResponse dataModelResponse = orchestrationService
-//        .deleteDataModel(collectionName, requestJson);
-//
-//    return ResponseEntity
-//        .status(dataModelResponse.getHttpStatus())
-//        .body(dataModelResponse.getStatusMessage());
-//  }
-
-//  @DeleteMapping
-//  public ResponseEntity<String> dropEntity(
-//      @RequestParam("tableName") String collectionName) {
-//
-//    DataModelResponse dataModelResponse = orchestrationService
-//        .createDataModel(collectionName, indexId, requestJson);
-//
-//    return ResponseEntity
-//        .status(dataModelResponse.getHttpStatus())
-//        .body(dataModelResponse.getStatusMessage());
-//  }
 }
